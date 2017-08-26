5f30ee8c650e5d046827c18cb48fb0b5543cda8a
MainMan <your_email@whatever.com>
2017-08-26 14:35:56 +0700

    Component life cycle

--- src/new.js
+++ src/new.js
@@ -4,9 +4,25 @@ export default class NewComponent extends Component {
 
     constructor(props) {
         super(props);
+
+        console.log('Constructor');
+    }
+
+    componentWillMount() {
+        console.log('componentWillMount');
+    }
+
+    componentDidMount() {
+        console.log('componentDidMount');
+    }
+
+    componentWillUnmount() {
+        console.log('componentWillUnmount');
     }
 
     render() {
+        console.log('render');
+
         return (
             <div style={{backgroundColor: 'red'}}>
                 <h3>New componenjt</h3>
@@ -15,3 +31,10 @@ export default class NewComponent extends Component {
         );
     }
 }
+
+/*
+Constructor
+componentWillMount
+render
+componentDidMount
+*/
